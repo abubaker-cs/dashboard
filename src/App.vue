0862a5a0db4203e1e867eb3fa2393d0ec60da242
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2020-10-06 19:16:51 +0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,16 @@
 </template>
 
 <style lang="scss">
+@import "@/global-styles/colors.scss";
+@import "@/global-styles/typography.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: yellow;
 }
 
 #nav {
